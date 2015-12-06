c83b14bbf372bc5799b77ea7d876a4391a71d5c3
CoRuGi <CR.Github.n6qc4@gmail.com>
2015-12-06 14:57:12 +0100

    Create MovieContract

---
+++ app/src/main/java/com/cr/androidnanodegree/popularmovies/data/MovieContract.java
@@ -0,0 +1,39 @@
+package com.cr.androidnanodegree.popularmovies.data;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Class that defines the tables and columns for the movie database.
+ */
+public class MovieContract {
+
+    // The content authority
+    public static final String CONTENT_AUTHORITY = "com.cr.androidnanodegree.popularmovies";
+
+    // Uri to contact the content provider
+    public static final Uri BASE_CONENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths
+    public static final String PATH_FAVORITES = "favorites";
+
+    public static final class FavoritesEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONENT_URI.buildUpon().appendEncodedPath(PATH_FAVORITES).build();
+
+        // Table name
+        public static final String COLUMN_TABLE_NAME = "favorites";
+
+        // ID of the movie
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+
+        // Path to the poster
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+
+        public static Uri buildFavoritesUri(long movieId) {
+            return ContentUris.withAppendedId(CONTENT_URI, movieId);
+        }
+    }
+}
